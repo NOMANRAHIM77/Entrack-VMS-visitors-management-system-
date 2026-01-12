923d679c191c2df10df648e291016912dc5f0b5d
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-12 22:27:20 +0500

    updated hospital.jsx

--- Entrack/src/softwareCategoriesComponents/Hospital.jsx
+++ Entrack/src/softwareCategoriesComponents/Hospital.jsx
@@ -4,8 +4,11 @@ const Hospital = () => {
   return (
     <>
     <div  className='w-full h-full flex justify-around gap-5 p-10 items-center ' >
-    <div  className='bg-yellow-300 rounded-4xl h-125 w-13' ></div>
-    <div className='bg-red-400 rounded-2xl w-250 h-122' ></div>
+    <div  className='bg-black rounded-4xl h-125 w-13' ></div>
+    <div className='bg-red-400 rounded-2xl w-250 h-122 flex flex-col ' >
+      <div className='w-full h-[50%] bg-purple-600 ' ></div>
+      <div  className='w-full h-[50%] bg-purple-300 '  ></div>
+    </div>
     </div>
     </>
   )
