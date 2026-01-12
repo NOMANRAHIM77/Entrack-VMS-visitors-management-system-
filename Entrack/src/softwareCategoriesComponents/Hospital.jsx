1a8cf9add680b7252bd24fd6fdd7dd3a0388b81f
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-12 23:11:17 +0500

    updated hospital.jsx

--- Entrack/src/softwareCategoriesComponents/Hospital.jsx
+++ Entrack/src/softwareCategoriesComponents/Hospital.jsx
@@ -7,11 +7,20 @@ const Hospital = () => {
     <div  className='bg-black rounded-4xl h-125 w-13' ></div>
     <div className='bg-red-400 rounded-2xl w-250 h-122 flex flex-col ' >
 
-      <div className='w-full h-[50%] bg-purple-600 flex flex-row gap-10 p-8' >
+      <div className='w-full h-[50%] bg-purple-600 flex flex-row gap-10 p-8 justify-around' >
 
-         <div className='w-10 h-10 rounded-2xl bg-white' ></div>
-        <div className='w-10 h-10 rounded-2xl bg-white' ></div>
-<div className='w-10 h-10 rounded-2xl bg-white' ></div>
+         <div className='w-50 h-40 rounded-2xl bg-white' >
+          <h1>Total visitors</h1>
+          <h2>0</h2>
+         </div>
+        <div className='w-50 h-40 rounded-2xl bg-white' >
+          <h1>Visitors Inside</h1>
+          <h2>0</h2>
+        </div>
+<div className='w-50 h-40 rounded-2xl bg-white' >
+  <h1>Visitors CheckedOut</h1>
+  <h2>0</h2>
+</div>
 
       </div>
       <div  className='w-full h-[50%] bg-purple-300 '  ></div>
