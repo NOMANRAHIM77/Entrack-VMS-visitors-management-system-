620112faa47d93ab4eee91690e3d0389bbea424c
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-12 22:13:40 +0500

    added hospital.jsx file

--- Entrack/src/softwareCategoriesComponents/Hospital.jsx
+++ Entrack/src/softwareCategoriesComponents/Hospital.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hospital = () => {
   return (
-    <div>HOSPITAL</div>
+    <>
+    <div  className='w-full h-full flex justify-around gap-5 p-10 items-center ' >
+    <div  className='bg-yellow-300 rounded-4xl h-125 w-13' ></div>
+    <div className='bg-red-400 rounded-2xl w-250 h-122' ></div>
+    </div>
+    </>
   )
 }
 
