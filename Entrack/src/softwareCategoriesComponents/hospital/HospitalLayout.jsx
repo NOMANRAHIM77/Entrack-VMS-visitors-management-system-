733e2daeecfde002d0fa8a6339a73a915804f458
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-21 00:44:54 +0500

    updated hospitallayout.jsx

--- Entrack/src/softwareCategoriesComponents/hospital/HospitalLayout.jsx
+++ Entrack/src/softwareCategoriesComponents/hospital/HospitalLayout.jsx
@@ -1,32 +1,11 @@
 // HospitalLayout.jsx
-import { NavLink, Outlet } from "react-router-dom";
+import HospitalDashboard from "./HospitalDashboard";
 
 const HospitalLayout = ({children}) => {
   return (
     <div className="min-h-screen bg-gray-100 flex">
       
-      {/* Sidebar */}
-      <aside className="w-64 bg-slate-900 text-white flex flex-col p-6">
-        <h1 className="text-2xl font-bold mb-10">Hospital Admin</h1>
-
-        <nav className="flex flex-col gap-4 text-gray-300">
-          <NavLink to="" end className="hover:text-white">
-            Dashboard
-          </NavLink>
-          <NavLink to="visitors" className="hover:text-white">
-            Visitors
-          </NavLink>
-          <NavLink to="patients" className="hover:text-white">
-            Patients
-          </NavLink>
-          <NavLink to="reports" className="hover:text-white">
-            Reports
-          </NavLink>
-          <NavLink to="settings" className="hover:text-white">
-            Settings
-          </NavLink>
-        </nav>
-      </aside>
+      <HospitalDashboard/>
 
       {/* Dynamic Content */}
       <main className="flex-1 p-8">
