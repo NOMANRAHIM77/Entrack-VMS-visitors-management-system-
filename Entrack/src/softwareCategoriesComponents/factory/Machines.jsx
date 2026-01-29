825149c5a158e474dbe66ecd2c51fef3509c4f0d
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-01-29 23:54:31 +0500

    added machine.jsx file

---
+++ Entrack/src/softwareCategoriesComponents/factory/Machines.jsx
@@ -0,0 +1,10 @@
+const Machines = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Machines</h1>
+      <p className="text-gray-600">List and manage factory machines.</p>
+    </div>
+  );
+};
+
+export default Machines;
