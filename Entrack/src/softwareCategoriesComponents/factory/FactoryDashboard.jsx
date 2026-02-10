acc4eaf9bb1add537d8834e74c95917d971eee3f
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-02-10 20:40:25 +0500

    added new file

--- Entrack/src/softwareCategoriesComponents/factory/FactoryDashboard.jsx
+++ Entrack/src/softwareCategoriesComponents/factory/FactoryDashboard.jsx
@@ -1,30 +1,42 @@
+import { Link } from "react-router-dom";
+
+
 const FactoryDashboard = () => {
   return (
-    <div className="space-y-6">
-      <h1 className="text-2xl font-bold">Factory Overview</h1>
+    <div className="min-h-screen flex bg-gray-100">
+      
+      {/* Left Sidebar */}
+      <aside className="w-64 bg-green-400 text-white p-6">
+        <h2 className="text-xl font-bold mb-8">Factory Dashboard</h2>
 
-      {/* Analytics Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-        <div className="bg-white p-6 rounded-xl shadow">
-          <p className="text-gray-500">Total Machines</p>
-          <h2 className="text-3xl font-bold">24</h2>
-        </div>
+        <nav className="flex flex-col gap-4 text-white-300">
+          <Link to="/factory/analytics" className="hover:text-white">
+           Analytics
+          </Link>
+          <Link to="/factory/machines" className="hover:text-white">
+            Machines
+          </Link>
+          <Link to="/factory/production" className="hover:text-white">
+            Production
+          </Link>
+          <Link to="/factory/reports" className="hover:text-white">
+            Reports
+          </Link>
+          <Link to="/factory/settings" className="hover:text-white">
+            Setings
+          </Link>
+          <Link to="/factory/workers" className="hover:text-white">
+            Workers
+          </Link>
+        </nav>
+      </aside>
 
-        <div className="bg-white p-6 rounded-xl shadow">
-          <p className="text-gray-500">Active Workers</p>
-          <h2 className="text-3xl font-bold text-green-600">120</h2>
-        </div>
+      {/* Right Content Area */}
+      <main className="flex-1 p-8">
+        
+        
 
-        <div className="bg-white p-6 rounded-xl shadow">
-          <p className="text-gray-500">Daily Production</p>
-          <h2 className="text-3xl font-bold">3,400</h2>
-        </div>
-
-        <div className="bg-white p-6 rounded-xl shadow">
-          <p className="text-gray-500">Faulty Machines</p>
-          <h2 className="text-3xl font-bold text-red-500">2</h2>
-        </div>
-      </div>
+      </main>
     </div>
   );
 };
