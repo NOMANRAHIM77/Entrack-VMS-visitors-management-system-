4c46f3a3ba8d832f30f829a7e925d734d508239d
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-02-10 20:40:58 +0500

    added new file

--- Entrack/src/softwareCategoriesComponents/factory/FactoryLayout.jsx
+++ Entrack/src/softwareCategoriesComponents/factory/FactoryLayout.jsx
@@ -1,31 +1,16 @@
-import { NavLink, Outlet } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import FactoryDashboard from "./FactoryDashboard";
 
-const FactoryLayout = () => {
+const FactoryLayout = ({ children }) => {
   return (
-    <div className="min-h-screen flex bg-gray-100">
-      
+    <div className="min-h-screen bg-gray-100 flex">
+
       {/* Sidebar */}
-      <aside className="w-64 bg-slate-900 text-white p-6">
-        <h2 className="text-xl font-bold mb-8">Factory Panel</h2>
-
-        <nav className="flex flex-col gap-4 text-gray-300">
-          <NavLink to="/factory" end className={({ isActive }) =>
-            isActive ? "text-white font-semibold" : "hover:text-white"
-          }>
-            Dashboard
-          </NavLink>
-
-          <NavLink to="/factory/machines">Machines</NavLink>
-          <NavLink to="/factory/workers">Workers</NavLink>
-          <NavLink to="/factory/production">Production</NavLink>
-          <NavLink to="/factory/reports">Reports</NavLink>
-          <NavLink to="/factory/settings">Settings</NavLink>
-        </nav>
-      </aside>
-
-      {/* Right Content */}
+       <FactoryDashboard/>
+      {/* Page Content */}
       <main className="flex-1 p-8">
-        <Outlet />
+        {children}
       </main>
     </div>
   );
