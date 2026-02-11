675600999f2b3697db883084fc6e13db2193b427
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-02-11 20:21:41 +0500

    added halls  file

---
+++ Entrack/src/softwareCategoriesComponents/eventhalls/Halls.jsx
@@ -0,0 +1,14 @@
+
+const Halls = () => {
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow">
+      <h3 className="text-2xl font-semibold mb-4">Hall Management</h3>
+
+      <div className="text-gray-500">
+        Add, edit and manage event halls here.
+      </div>
+    </div>
+  );
+};
+
+export default Halls;
