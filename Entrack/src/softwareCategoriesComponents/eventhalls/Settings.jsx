524e335e49dce3bd7f184481af56efefd72707e3
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-02-11 20:22:13 +0500

    added settings  file

---
+++ Entrack/src/softwareCategoriesComponents/eventhalls/Settings.jsx
@@ -0,0 +1,13 @@
+const Settings = () => {
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow">
+      <h3 className="text-2xl font-semibold mb-4">Settings</h3>
+
+      <p className="text-gray-500">
+        Configure event hall system settings here.
+      </p>
+    </div>
+  );
+};
+
+export default Settings;
