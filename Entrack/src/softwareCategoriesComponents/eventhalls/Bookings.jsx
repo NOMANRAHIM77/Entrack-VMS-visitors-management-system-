4e18b6a84a13f043ec1ddeb01668e93c3db3cd1e
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-02-11 20:18:34 +0500

    added Bookings file

---
+++ Entrack/src/softwareCategoriesComponents/eventhalls/Bookings.jsx
@@ -0,0 +1,13 @@
+const Bookings = () => {
+  return (
+    <div className="bg-white p-6 rounded-2xl shadow">
+      <h3 className="text-2xl font-semibold mb-4">Bookings Management</h3>
+
+      <div className="text-gray-500">
+        List of all event bookings will appear here.
+      </div>
+    </div>
+  );
+};
+
+export default Bookings;
